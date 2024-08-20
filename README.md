799225a45388662de622ce5a560a945a1bd90515
wide_by_wide <antonsyski@gmail.com>
2024-08-20 23:21:25 +0300

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Malestones
+## 0.1.0
+
+- [ ] Infrastucture
+    - [ ] Foramter
+    - [x] Build
+    - [ ] License
+    - [ ] Versions
---
+++ include/core.hxx
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+
+namespace yaga
+{
+    class core
+    {
+    public:
+        core(/* args */){}
+        ~core(){}
+
+        const std::uint32_t get_minor_version();
+        const std::uint32_t get_major_version();
+        const std::uint32_t get_patch_version();
+    };
+} // namespace engine
